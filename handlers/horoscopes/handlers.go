919948d101f19e250a0db0823b9d9b4de9b0819a
Arlen Nascimento <arlen.nascimento@gmail.com>
2019-09-26 09:45:53 -0400

    fixing response

--- handlers/horoscopes/handlers.go
+++ handlers/horoscopes/handlers.go
@@ -68,8 +68,9 @@ func (h *Handler) GetHoroscope(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	log.Println("Sending 200")
 	//we should tell slack asap that we received the request to avoid timeout error
-	w.WriteHeader(http.StatusOK)
+	w.Write(helpers.JSON(helpers.IDResponse{ID:200}))
 
 	now := time.Now().Format("2006-01-02")
 	redisKey := fmt.Sprintf("%s:%s:%s", sunsign, period, now)
